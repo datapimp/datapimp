8f32bd3cab05cbc5d3cfc568f845f74ee65ac861
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-11-25 02:09:21 -0600

    updating etag method

--- lib/datapimp/filterable/cached_context.rb
+++ lib/datapimp/filterable/cached_context.rb
@@ -1,12 +1,10 @@
 module Datapimp
   module Filterable
     class CachedContext < Context
-      def cache_key
-        base  = scope.scope_attributes.inject([scope.klass.to_s]) {|m,k| m << k.map(&:to_s).map(&:strip).join(':') }
-        parts = params.inject(base) {|m,k| m << k.map(&:to_s).map(&:strip).join(':') }
-        key   = parts.sort.uniq.join('/')
-
-        "#{ key }/#{ scope.maximum(:updated_at).to_i }/#{ scope.count }"
+      def etag
+        Rails.cache.fetch("etags:#{cache_key}") do
+          Digest::MD5.digest(wrap_results.as_json.to_json)
+        end
       end
 
       def execute
