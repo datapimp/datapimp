4ddbd565fd16d803f738412b1e0f76fa2f5fc8ce
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-12-11 00:08:05 -0600

    controller specs - after create, after update success callbacks

--- spec/controllers/controller_mixins_spec.rb
+++ spec/controllers/controller_mixins_spec.rb
@@ -1,6 +1,7 @@
 require "spec_helper"
 
 describe "The Controller Mixins" do
+
   before(:all) { @controller = ProjectsController.new }
 
   let(:sample) { Project.last }
@@ -21,5 +22,16 @@ describe "The Controller Mixins" do
     post :create, :format => :json, :project => {name:"soederpop"}
     Project.where(name:"soederpop").should_not be_empty
     response.should be_success
+  end
+
+  it "should update a record" do
+    ProjectsController.any_instance.should_receive(:after_update_success)
+    put :update, id: sample.id, :format => :json, :project => {name:"boom son"}
+    response.should be_success
+  end
+
+  it "should call the after create success callback" do
+    ProjectsController.any_instance.should_receive(:after_create_success)
+    post :create, :format => :json, :project => {name:"soederpop"}
   end
 end
